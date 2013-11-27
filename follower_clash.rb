79b57591401db652ed0f9e0d09c50cf0b4a291d3
erlaroche <elaroche@Athena.local>
2013-11-27 11:29:26 -0600

    Add third method to compare tweet count

--- follower_clash.rb
+++ follower_clash.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'pry'
 
 require_relative 'lib/user_comparar'
 
@@ -10,3 +11,7 @@ puts "#{user2.username}'s follower count: #{user2.followers}"
 
 comparer = UserComparer::Comparer.new(user1, user2)
 puts "And the user with the most followers is: #{comparer.compare}"
+
+puts "And the user with the most friends is: #{comparer.friend_count}"
+
+puts "And the user with the most tweets is: #{comparer.tweet_count}"
--- lib/user_comparar.rb
+++ lib/user_comparar.rb
@@ -4,7 +4,7 @@ require 'twitter'
 
 module UserComparer
   class User
-  attr_reader :username
+    attr_reader :username
     
     def initialize(username)
       @username=username
@@ -12,17 +12,33 @@ module UserComparer
 
     def followers
       client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['CONSUMER_KEY']
-      config.consumer_secret     = ENV['CONSUMER_SECRET']
-      config.access_token         = ENV['OAUTH_TOKEN']
-      config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
+        config.consumer_key        = ENV['CONSUMER_KEY']
+        config.consumer_secret     = ENV['CONSUMER_SECRET']
+        config.access_token         = ENV['OAUTH_TOKEN']
+        config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
       end
-      
-
       client.user(@username).followers_count
-      # Now, we can call both username and followers on a UserCompaper::User object
-      # UserComparer::User.username
-      # UserComparer::User.followers
+    end
+
+    def friends
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV['CONSUMER_KEY']
+        config.consumer_secret     = ENV['CONSUMER_SECRET']
+        config.access_token         = ENV['OAUTH_TOKEN']
+        config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
+      end
+      client.user(@username).friends_count
+    end
+  
+
+    def tweets
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV['CONSUMER_KEY']
+        config.consumer_secret     = ENV['CONSUMER_SECRET']
+        config.access_token         = ENV['OAUTH_TOKEN']
+        config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
+      end
+      client.user(@username).tweets_count
     end
   end
 
@@ -37,9 +53,29 @@ module UserComparer
       if (@user1.followers) == (@user2.followers)
         return "#{user1.username} and #{user2.username} have a tie in followers!"
       elsif (@user1.followers) > (@user2.followers)
-        return "#{@user1.username} has more followers."
+        return "#{@user1.username} has more followers than #{@user2.username}."
       else (@user1.followers) < (@user2.followers)
         return "#{@user2.username} has more followers."
+      end
+    end
+
+    def friend_count
+      if (@user1.friends) == (@user2.friends)
+        return "#{user1.username} and #{user2.username} have a tie in followers!"
+      elsif (@user1.friends) > (@user2.friends)
+        return "#{@user1.username} has more friends than #{@user2.username}."
+      else (@user1.friends) < (@user2.friends)
+        return "#{@user2.username} has more followers than #{@user1.username}."
+      end
+    end
+
+    def tweet_count
+      if (@user1.tweets) == (@user2.tweets)
+        return "#{@user1.tweets} and #{@user2.tweets} have a tie in followers!"
+      elsif (@user1.tweets) > (@user2.tweets)
+        return "#{@user1.username} has more friends than #{@user2.username}."
+      else (@user1.tweets) < (@user2.tweets)
+        return "#{@user2.username} has more followers than #{@user1.username}."
       end
     end
   end
